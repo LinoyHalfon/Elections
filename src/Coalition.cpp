61c9b1ccc8806cfe687b26a0147962fe27a42da7
LinoyHalfon <119005868+LinoyHalfon@users.noreply.github.com>
2022-12-16 18:31:48 +0200

    Update Coalition.cpp

--- src/Coalition.cpp
+++ src/Coalition.cpp
@@ -6,7 +6,6 @@ Coalition::Coalition(int id, int partyId, int mandates) : mId(id), mParties(), m
     parties.push_back(partyId);
     mParties = parties;
 }
-///hert
 
 const int Coalition::getId() const
 {
